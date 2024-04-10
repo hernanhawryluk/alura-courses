83699af459073892cca5512c82d42f2b1a46e2a2
Hernan Hawryluk <hernanhawryluk@gmail.com>
2024-04-10 18:11:22 -0300

    First classes on Java

--- screenmatch/src/Main.java
+++ screenmatch/src/Main.java
@@ -1,5 +1,22 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Movie myMovie = new Movie();
+        myMovie.name = "Encanto";
+        myMovie.launchDate = 2021;
+        myMovie.durationInMinutes = 120;
+
+        myMovie.displayTechnicalSpecifications();
+        myMovie.evaluate(10);
+        myMovie.evaluate(7.8);
+        System.out.println("The sum of the evaluations is: " + myMovie.sumOfEvaluations);
+        System.out.println("The total of evaluations is: " + myMovie.totalOfEvaluations);
+        System.out.println("The rating of the movie is: " + myMovie.calcMedia());
+
+        Movie otherMovie = new Movie();
+        otherMovie.name = "Matrix";
+        otherMovie.launchDate = 1998;
+        otherMovie.durationInMinutes = 180;
+//        otherMovie.displayTechnicalSpecifications();
+
     }
 }
---
+++ screenmatch/src/Movie.java
@@ -0,0 +1,23 @@
+public class Movie {
+    String name;
+    int launchDate;
+    int durationInMinutes;
+    boolean includedInThePlan;
+    double sumOfEvaluations;
+    int totalOfEvaluations;
+
+    void displayTechnicalSpecifications() {
+        System.out.println("The name of the movie is: " + name);
+        System.out.println("It was launch on: " + launchDate);
+        System.out.println("The duration in minutes is: " + durationInMinutes);
+    }
+
+    void evaluate(double note) {
+        sumOfEvaluations += note;
+        totalOfEvaluations++;
+    }
+
+    double calcMedia() {
+        return sumOfEvaluations / totalOfEvaluations;
+    }
+}
