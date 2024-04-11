30aa0556e6629de6116850ef96f969c402f0a660
Hernan Hawryluk <hernanhawryluk@gmail.com>
2024-04-11 14:15:45 -0300

    Added inheritance to Movies and Series classes

--- screenmatch/src/com/aluracursos/screenmatch/modelos/Movie.java
+++ screenmatch/src/com/aluracursos/screenmatch/modelos/Movie.java
@@ -1,57 +1,13 @@
 package com.aluracursos.screenmatch.modelos;
 
-public class Movie {
-    private String name;
-    private int launchDate;
-    private int durationInMinutes;
-    private boolean includedInThePlan;
-    private double sumOfEvaluations;
-    private int totalOfEvaluations;
+public class Movie extends Title {
+    private String director;
 
-    public String getName() {
-        return name;
+    public String getDirector() {
+        return director;
     }
 
-    public int getLaunchDate() {
-        return launchDate;
-    }
-
-    public int getDurationInMinutes() {
-        return durationInMinutes;
-    }
-
-    public boolean isIncludedInThePlan() {
-        return includedInThePlan;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public void setLaunchDate(int launchDate) {
-        this.launchDate = launchDate;
-    }
-
-    public void setDurationInMinutes(int durationInMinutes) {
-        this.durationInMinutes = durationInMinutes;
-    }
-
-    public int getTotalOfEvaluations() {
-        return totalOfEvaluations;
-    }
-
-    public void displayTechnicalSpecifications() {
-        System.out.println("The name of the movie is: " + name);
-        System.out.println("It was launch on: " + launchDate);
-        System.out.println("The duration in minutes is: " + durationInMinutes);
-    }
-
-    public void evaluate(double note) {
-        sumOfEvaluations += note;
-        totalOfEvaluations++;
-    }
-
-    public double calcMedia() {
-        return sumOfEvaluations / totalOfEvaluations;
+    public void setDirector(String director) {
+        this.director = director;
     }
 }
---
+++ screenmatch/src/com/aluracursos/screenmatch/modelos/Series.java
@@ -0,0 +1,31 @@
+package com.aluracursos.screenmatch.modelos;
+
+public class Series extends Title {
+    int seasons;
+    int episodesPerSeason;
+    int minutesPerEpisode;
+
+    public int getSeasons() {
+        return seasons;
+    }
+
+    public void setSeasons(int seasons) {
+        this.seasons = seasons;
+    }
+
+    public int getEpisodesPerSeason() {
+        return episodesPerSeason;
+    }
+
+    public void setEpisodesPerSeason(int episodesPerSeason) {
+        this.episodesPerSeason = episodesPerSeason;
+    }
+
+    public int getMinutesPerEpisode() {
+        return minutesPerEpisode;
+    }
+
+    public void setMinutesPerEpisode(int minutesPerEpisode) {
+        this.minutesPerEpisode = minutesPerEpisode;
+    }
+}
