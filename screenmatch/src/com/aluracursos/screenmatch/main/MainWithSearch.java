e1500783e0bcfc0be1a92261f0f09898e7c863a3
Hernan Hawryluk <hernanhawryluk@gmail.com>
2024-04-13 12:52:05 -0300

    Added a http request to consume an API

---
+++ screenmatch/src/com/aluracursos/screenmatch/main/MainWithSearch.java
@@ -0,0 +1,19 @@
+package com.aluracursos.screenmatch.main;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
+public class MainWithSearch {
+    public static void main(String[] args) throws IOException, InterruptedException {
+        HttpClient client = HttpClient.newHttpClient();
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create("http://www.omdbapi.com/?t=matrix&apikey=27222298"))
+                .build();
+        HttpResponse<String> response = client
+                .send(request, HttpResponse.BodyHandlers.ofString());
+        System.out.println(response.body());
+    }
+}
