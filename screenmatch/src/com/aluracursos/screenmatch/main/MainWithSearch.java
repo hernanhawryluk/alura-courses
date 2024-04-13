5ef79d44b01357a98140338872120480c69e71d2
Hernan Hawryluk <hernanhawryluk@gmail.com>
2024-04-13 13:15:27 -0300

    Added search function for the movie

--- screenmatch/src/com/aluracursos/screenmatch/main/MainWithSearch.java
+++ screenmatch/src/com/aluracursos/screenmatch/main/MainWithSearch.java
@@ -5,12 +5,19 @@ import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
+import java.util.Scanner;
 
 public class MainWithSearch {
     public static void main(String[] args) throws IOException, InterruptedException {
+        Scanner prompt = new Scanner(System.in);
+        System.out.println("Write the movie you are looking for:");
+        var search = prompt.nextLine();
+
+        String url = "http://www.omdbapi.com/?t=" + search + "&apikey=27222298";
+
         HttpClient client = HttpClient.newHttpClient();
         HttpRequest request = HttpRequest.newBuilder()
-                .uri(URI.create("http://www.omdbapi.com/?t=matrix&apikey=27222298"))
+                .uri(URI.create(url))
                 .build();
         HttpResponse<String> response = client
                 .send(request, HttpResponse.BodyHandlers.ofString());
